2cfad8ce812d3c995ad7b353c7b129c75ac9c82d
Emaro <ma@ava.com>
2025-11-14 18:14:48 +0100

    Update vehicle behavior

--- scripts/vehicle_body_3d.gd
+++ scripts/vehicle_body_3d.gd
@@ -10,8 +10,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+@export var max_rpm = 450
+@export var max_torque = 300
+@export var turn_speed = 3
+@export var turn_amound = 0.3
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("move_right", "move_left"), delta * 50)
-	engine_force = Input.get_axis("move_back", "move_forward") * 5000
-	if (Input.get_axis("move_back", "move_forward") > 0):
-		print(Input.get_axis("move_back", "move_forward"))
+	var dir = Input.get_axis("move_back", "move_forward")
+	var steering_dir = Input.get_axis("move_right", "move_left")
+	var rpm = abs($wheelBR.get_rpm() + $wheelBL.get_rpm()) / 2.0
+	var torque = dir * max_torque * (1.0 - rpm / max_rpm)
+	
+	engine_force = torque
+	steering = lerp(steering, steering_dir * turn_amound, turn_speed * delta)
