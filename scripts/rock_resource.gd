a9e7a1893f41279589a3f8b7a4a45ce800c92e21
Dobrawa Kiefer <dobrawa.kiefer@students.fhnw.ch>
2025-11-17 14:09:13 +0100

    placed new resources

---
+++ scripts/rock_resource.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+@export var item_id := "rock"
+@export var amount := 1
+var picked := false
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node) -> void:
+	if picked:
+		return
+	if body.name == "Rover" or body is CharacterBody3D:
+		Inventory.add_item(item_id, amount)
+		picked = true
+		queue_free()
