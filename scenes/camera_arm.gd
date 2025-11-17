11b4c56ad5bde12bb0c097ab41a15f4091a429ac
Emaro <ma@ava.com>
2025-11-17 11:00:12 +0100

    Limit camera movement when AlienDialog is open

--- scenes/camera_arm.gd
+++ scenes/camera_arm.gd
@@ -3,10 +3,18 @@ extends Camera3D
 @export var min_dist := 4.0
 @export var max_dist := 8.0
 @export var height := 3.0
+@export var camera_sensibility := 0.001
+@export var hud : Node
 
-@onready var target : Node3D = get_parent()
+@onready var target : Node3D = get_parent().get_parent()
 
-func  _physics_process(delta: float) -> void:
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and !hud.get_node("AlienDialog").visible:
+		top_level = false
+		get_parent().rotate_y(-event.relative.x * camera_sensibility)
+		top_level = true
+		
+func _physics_process(delta: float) -> void:
 	var from_target := global_position - target.global_position
 	
 	if from_target.length() < min_dist:
